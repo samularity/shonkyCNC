87c063cb6bd74ae106ee601c3e2369ccb0ad8505
samularity <samuelmunz@gmx.de>
2014-01-28 12:23:01 +0100

    Revert "formatting"
    This reverts commit faeee73d54d5db294a93913179658df8193cd340.

--- source/main.c
+++ source/main.c
@@ -10,6 +10,7 @@ PinOut TinyMega (aka JockiBoard)  :	http://shackspace.de/wiki/doku.php?id=projec
 
 USB-Serial Device with command line
 
+
 B0,1,2,3 Driver X Axis
 B4,5,6,7 Driver Y Axis
 C6:		Enable/Disable Drivers
@@ -19,6 +20,9 @@ D4,5,6,7 Driver Z Axis
 D2:		Seriall_RX
 F0:		General Analog Input for Toolspeed etc.
 F1:		Switch Matrix Analog input
+
+
+
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 /*/////////////////////////////////////////////////////////////////////////////////////////////////////////
