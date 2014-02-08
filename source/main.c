d2b5a2da44df271f5e828265cfa79e5c10a5ce5a
samularity <samularity@users.noreply.github.com>
2014-02-08 01:50:28 +0100

    Disable JTAG
    unable to disable pullup resistors with jtag turned on

--- source/main.c
+++ source/main.c
@@ -42,10 +42,11 @@ int main(void)
 {
 	clock_prescale_set(clock_div_1);	//clock prescaler auf  1 =>  16MHz
 	MCUCR = (1 << JTD);					//jtag ausschalten
+	MCUCR = (1 << JTD);					//this need to be executed twice 
 	
 	DDRE|=(1 << PE6); //debug led als ausgang
 	DDRB = 0xff;//alle als ausgang zu treibern x+y
-	DDRD |= (1 << DDD4) | (1 << DDD5)| (1 << DDD6)| (1 << DDD7); //ausgänge für z achse
+	DDRD |= (1 << DDD4) | (1 << DDD5)| (1 << DDD6)| (1 << DDD7); //ausgï¿½nge fï¿½r z achse
 	
 	DDRC |= (1 << DDC7) ; //Pin C7 als PWM ausgang
 	DDRC |= (1 << DDC6) ; //Pin C6 als ENABLE/DISLABLE Stepper an L293D
@@ -61,7 +62,7 @@ int main(void)
 	//interupt0 test
 	DDRD &= ~(1 << DDD0); // pin d0 als eingang (interrupt notaus)
 	PORTD |= (1 << PD0); // pullup aktiviern
-	EICRA &= ~(  (1<<ISC00) | (1<<ISC01)  ); // sets the interrupt type bei low interrupt auslösen
+	EICRA &= ~(  (1<<ISC00) | (1<<ISC01)  ); // sets the interrupt type bei low interrupt auslï¿½sen
 	sleep_ms(10);
 	EIMSK |= (1<<INT0); // activiern des interrupts
 	sleep_ms(10);
@@ -76,15 +77,15 @@ while(1)
 	  usb_serial_putchar(inputbuffer[counter]);
 	  usb_serial_flush_input();
 	  counter++;
-	  if ( (counter>= 20) || (inputbuffer[counter-1]==13) ) //sind 20 zeichern erreicht oder enter gerückt
+	  if ( (counter>= 20) || (inputbuffer[counter-1]==13) ) //sind 20 zeichern erreicht oder enter gerï¿½ckt
 	   {
 		inputbuffer[counter-1]='\0'; //enter entfernen und string terminnieren
 		
 		 cmd_handler(inputbuffer);
 		 memset	(&inputbuffer,0,250);//string leeren
-		counter =0; //zähler zurücksetzten string soll wieder ab 0 gefüllt werden
+		counter =0; //zï¿½hler zurï¿½cksetzten string soll wieder ab 0 gefï¿½llt werden
 		}//if stringende 
-	  }//if usbserial availaböle						
+	  }//if usbserial availabï¿½le						
     }//while 1
 }//main
 
@@ -107,8 +108,8 @@ void sleep_ms(uint16_t ms){
 
 void bootloader (void)
 {
-	MCUCR  |=  (1 << IVCE);  //IVCE  = 1		//register für restret
-	MCUCR  |=  (1 << IVSEL); //IVSEL = 1		//register für restret
+	MCUCR  |=  (1 << IVCE);  //IVCE  = 1		//register fï¿½r restret
+	MCUCR  |=  (1 << IVSEL); //IVSEL = 1		//register fï¿½r restret
 	TIMSK0 = 0;             //Timer-Interrupt ausschalten
 	TIMSK1 = 0;				//noch ein Timer-Interrupt ausschalten
 	TIMSK4 = 0;				//und noch einer
@@ -117,7 +118,7 @@ void bootloader (void)
 }
 
 
-uint8_t get_switch(void) //lese adc und ermittle welche taste gerückt wurde
+uint8_t get_switch(void) //lese adc und ermittle welche taste gerï¿½ckt wurde
 {
 	uint16_t input = readADC(1);
 /*
